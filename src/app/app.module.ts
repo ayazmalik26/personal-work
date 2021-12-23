adca647da751f243d8966477f14451e3953f9fcc
Ayaz Malik <ayazaslam400@gmail.com>
2021-12-23 14:45:13 +0500

    changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LogInComponent } from './main/login/log-in/log-in.component';
+import { SignUpComponent } from './main/sign-up/sign-up.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LogInComponent,
+    SignUpComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FlexLayoutModule ,
+    
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
